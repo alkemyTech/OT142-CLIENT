a818870ac895479f90bca092606314bb4e1c0fc8
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-02-17 20:29:43 -0300

    Starting the form edition and user create

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import '../FormStyles.css';
+
+const SignupSchema = Yup.object().shape({
+    name: Yup.string().email('Invalid email').required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password:Yup.string().email('Invalid email').required('Required'),
+    roleId: Yup.string().email('Invalid email').required('Required')
+  });
 
 const UserForm = () => {
     const [initialValues, setInitialValues] = useState({
         name: '',
         email: '',
+        password: '',
         roleId: ''
     })
 
@@ -22,16 +32,20 @@ const UserForm = () => {
     }
 
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name || ''} onChange={handleChange} placeholder="Name"></input>
-            <input className="input-field" type="text" name="email" value={initialValues.description || ''} onChange={handleChange} placeholder="Email"></input>
-            <select className="input-field" value={initialValues.roleId || ''} onChange={e => setInitialValues({...initialValues, roleId: e.target.value})}>
-                <option value="" disabled >Select the role</option>
-                <option value="1">Admin</option>
-                <option value="2">User</option>
-            </select>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
+        <Formik>
+            <form className="form-container" onSubmit={handleSubmit}>
+                <input className="input-field" type="file"></input>
+                <input className="input-field" type="text" name="name" value={initialValues.name || ''} onChange={handleChange} placeholder="Name"></input>
+                <input className="input-field" type="text" name="email" value={initialValues.email || ''} onChange={handleChange} placeholder="Email"></input>
+                <input className="input-field" type="password" name="password" value={initialValues.password || ''} onChange={handleChange} placeholder="Password"></input>
+                <select className="input-field" value={initialValues.roleId || ''} onChange={e => setInitialValues({...initialValues, roleId: e.target.value})}>
+                    <option value="" disabled >Select the role</option>
+                    <option value="1">Admin</option>
+                    <option value="2">User</option>
+                </select>
+                <button className="submit-btn" type="submit">Send</button>
+            </form>
+        </Formik>
     );
 }
  
