e0a446c347e81b2a8d6f6d7e59793c568b9bf89a
ncaruso <nico.caruso1992@outlook.com>
2022-02-17 18:08:55 -0300

    Integracion con chakra ui

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import "../FormStyles.css";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import {
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 
 const initialValues = {
   email: "",
@@ -10,81 +17,85 @@ const initialValues = {
 };
 
 const onSubmit = (values) => {
-    const user = {...values};
+  const user = { ...values };
 
-    localStorage.setItem("token", "tokenValueExample");
+  localStorage.setItem("token", "tokenValueExample");
 };
 
-
-
 const validationSchema = Yup.object({
-
-  email: Yup.string().email("Invalid email format").required("Please enter your email"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Please enter your email"),
   password: Yup.string()
     .required("Please enter your password")
     .trim()
     .min(6, "Must be at least 6 characters")
     .matches(
-        /^[0-9A-Za-z]*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?][0-9a-zA-Z]*$/,
-       'Need one special character or number',
+      /^[0-9A-Za-z]*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?][0-9a-zA-Z]*$/,
+      "Need one special character or number"
     ),
   passwordRepeat: Yup.string()
     .required("Please enter your password")
-    .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
 });
 
 const RegisterForm = () => {
-  
-
   const formik = useFormik({
     initialValues,
     onSubmit,
     validationSchema,
   });
 
-
-
   return (
     <form className="form-container" onSubmit={formik.handleSubmit}>
-      <input
-        className="input-field"
-        type="email"
-        name="email"
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="Enter email"
-      ></input>
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
-      <input
-        className="input-field"
-        type="password"
-        name="password"
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="Enter your password"
-      ></input>
-      {formik.touched.password && formik.errors.password ? (
-        <div>{formik.errors.password}</div>
-      ) : null}
-      <input
-        className="input-field"
-        type="password"
-        name="passwordRepeat"
-        value={formik.values.passwordRepeat}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder="Repeat your password"
-      ></input>
-      {formik.touched.passwordRepeat && formik.errors.passwordRepeat ? (
-        <div>{formik.errors.passwordRepeat}</div>
-      ) : null}
-      <button className="submit-btn" type="submit">
+      <FormControl isInvalid={formik.errors.email}>
+        <FormLabel htmlFor="email">Email address</FormLabel>
+        <Input
+          variant="outline"
+          type="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Enter email"
+        ></Input>
+        {formik.touched.email && formik.errors.email ? (
+          <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+        ) : null}
+      </FormControl>
+      <FormControl isInvalid={formik.errors.password}>
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input
+          variant="outline"
+          type="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Enter your password"
+        ></Input>
+        {formik.touched.password && formik.errors.password ? (
+          <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+        ) : null}
+      </FormControl>
+      <FormControl isInvalid={formik.errors.passwordRepeat}>
+        <FormLabel htmlFor="passwordRepeated">Repeat your password</FormLabel>
+        <Input
+          variant="outline"
+          type="password"
+          name="passwordRepeat"
+          value={formik.values.passwordRepeat}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Repeat your password"
+        ></Input>
+        {formik.touched.passwordRepeat && formik.errors.passwordRepeat ? (
+          <FormErrorMessage>{formik.errors.passwordRepeat}</FormErrorMessage>
+        ) : null}
+      </FormControl>
+      <Button colorScheme="blue" size="md" type="submit">
         Register
-      </button>
+      </Button>
     </form>
   );
 };
