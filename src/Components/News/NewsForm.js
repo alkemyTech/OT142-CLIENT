d146ef97bccae63b56151e1c7a32c625049f5269
Gabriel Rea <gabriel.rea96@gmail.com>
2022-02-18 20:16:52 -0300

    Primera version del form

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -1,39 +1,108 @@
-import React, { useState } from 'react';
-import '../../Components/FormStyles.css';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { 
+    Button, 
+    Stack,
+    Input,
+    Select,
+    Img
+} from '@chakra-ui/react'
+import axios from 'axios';
+import '../FormStyles.css';
 
 const NewsForm = () => {
     const [initialValues, setInitialValues] = useState({
         title: '',
         content: '',
-        category: ''
+        category: '',
+        image: ''
     });
 
-    const handleChange = (e) => {
-        if(e.target.name === 'title'){
-            setInitialValues({...initialValues, title: e.target.value})
-        } if(e.target.name === 'content'){
-            setInitialValues({...initialValues, content: e.target.value})
-        } if(e.target.name === 'category') {
-            setInitialValues({...initialValues, category: e.target.value})
+    const { id } = useParams();
+
+    useEffect(() => {
+
+        const URL = 'http://ongapi.alkemy.org/api/categories'
+
+        const getCategory = async() => {
+
+            const res = await axios.get(`${URL}/${id}`)
+            const {data} = await res.data;
+
+            setInitialValues({
+                title: data.name,
+                content: data.description,
+                category: '',
+                image: data.image
+            })
         }
+
+        if(id){
+            getCategory()
+        }
+
+    }, [id])
+
+    const handleChange = (e) => {
+        setInitialValues({
+            ...initialValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleContent = (e, editor) => {
+        const data = editor.getData();
+        
+        setInitialValues({
+            ...initialValues,
+            content: data 
+        })
+    }
+
+    const handleImage = (e) =>{
+        setInitialValues({
+            ...initialValues,
+            image: e.target.files[0].name 
+        })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(initialValues);
+        alert(JSON.stringify(initialValues, null, 2));
     }
+
+    console.log(initialValues)
 
     return (
         <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="title" value={initialValues.title || ''} onChange={handleChange}></input>
-            <input className="input-field" type="text" name="content" value={initialValues.content || ''} onChange={handleChange}></input>
-            <select className="select-field" name="category" value={initialValues.category || ''} onChange={handleChange}>
-                <option value="" disabled>Select category</option>
-                <option value="1">Demo option 1</option>
-                <option value="2">Demo option 2</option>
-                <option value="3">Demo option 3</option>
-            </select>
-            <button className="submit-btn" type="submit">Send</button>
+            <Stack spacing={13} p={10}>
+                <Input name="title" value={initialValues.title} onChange={handleChange} mb={2} />
+                <CKEditor
+                        editor={ ClassicEditor }
+                        // data={initialValues.content}
+                        onChange={handleContent}
+                />
+                <Select name="category" value={initialValues.category} onChange={handleChange}>
+                    <option value="" disabled>Select category</option>
+                    <option value="1">Demo option 1</option>
+                    <option value="2">Demo option 2</option>
+                    <option value="3">Demo option 3</option>
+                </Select>
+                <input type='file' onChange={handleImage}></input>
+                {
+                    initialValues.image !== '' &&
+                        <Img 
+                            src={initialValues.image} 
+                            alt={initialValues.title}
+                            boxSize='150px'
+                            objectFit='cover'
+                            mb={2}
+                        />
+                }
+                <Button colorScheme="blue" type="submit">Send</Button>
+            </Stack> 
         </form>
     );
 }
