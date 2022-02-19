95fcf204cb7fb910b316effd38a0feefacbbcf2b
Mauro Erben <mauroerben100@hotmail.com>
2022-02-18 23:33:02 -0300

    New projects form

--- src/Components/Projects/ProjectsForm.js
+++ src/Components/Projects/ProjectsForm.js
@@ -1,32 +1,125 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React, { useState, useEffect } from 'react';
+import {
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  Button,
+  FormErrorMessage,
+  InputGroup,
+  InputLeftElement,
+  Icon
+} from '@chakra-ui/react';
+import { Formik } from 'formik';
+import { ProjectsSchema } from './ProjectsSchema';
+import { MdTitle, MdImage } from 'react-icons/md';
 
-const ProjectsForm = () => {
-  const [initialValues, setInitialValues] = useState({
-    title: '',
-    description: ''
-  })
+const ProjectsForm = ({ patchObject }) => {
 
-  const handleChange = (e) => {
-    if(e.target.name === 'title'){
-      setInitialValues({...initialValues, title: e.target.value})
-    } if(e.target.name === 'description'){
-      setInitialValues({...initialValues, description: e.target.value})
-    }
-  }
+  //Por ahora guardamos los valores en estado hasta implementar peticiones
+  const [inputValues, setInputValues] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValues);
-  }
+  useEffect(() => {
+    console.log(inputValues);
+  }, [inputValues])
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input className="input-field" type="text" name="title" value={initialValues.title} onChange={handleChange} placeholder="Title"></input>
-      <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some description"></input>
-      <button className="submit-btn" type="submit">Send</button>
-    </form>
+    <Flex justifyContent="center" alignItems="center" flexDirection="column-reverse">
+
+      <Box p="8">
+
+        <Box textAlign="center">
+          <Heading size="md">Manage projects</Heading>
+        </Box>
+
+        <Box my="4">
+          <Formik
+            initialValues={{ title: '', description: '', image: '', date: '' }}
+
+            validationSchema={ProjectsSchema}
+
+            onSubmit={(values, action) => {
+              setInputValues(values);
+              action.setSubmitting(false);
+              action.resetForm();
+            }}
+          >
+            {({ handleSubmit, handleChange, handleBlur, values, errors, isSubmitting }) => (
+              <form onSubmit={handleSubmit}>
+                <FormControl isRequired isInvalid={errors.title}>
+                  <FormLabel>Title</FormLabel>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<Icon as={MdTitle} />}
+                    />
+                    <Input
+                      type="text"
+                      name='title'
+                      placeholder="Title"
+                      value={values.title}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </InputGroup>
+                  {errors.title && <FormErrorMessage p="2" bg="red.100">{errors.title}</FormErrorMessage>}
+                </FormControl>
+
+                <FormControl mt="4" isRequired isInvalid={errors.description}>
+                  <FormLabel>Description</FormLabel>
+                  <Textarea
+                    name='description'
+                    placeholder=' Message'
+                    value={values.description}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.description && <FormErrorMessage p="2" bg="red.100">{errors.description}</FormErrorMessage>}
+                </FormControl>
+
+                <FormControl mt="4" isRequired isInvalid={errors.image}>
+                  <FormLabel>Image</FormLabel>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<Icon as={MdImage} />}
+                    />
+                    <Input
+                      type="file"
+                      name='image'
+                      accept='.png, .jpg'
+                      value={values.image}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </InputGroup>
+                  {errors.image && <FormErrorMessage p="2" bg="red.100">{errors.image}</FormErrorMessage>}
+                </FormControl>
+
+                <FormControl mt="4">
+                  <FormLabel>Due date</FormLabel>
+                  <Input
+                    type="date"
+                    name='date'
+                    value={values.date}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </FormControl>
+
+                <Button colorScheme="blue" mt="4" w="100%" type='Submit'>
+                  {isSubmitting ? 'Submitting...' : 'Submit'}
+                </Button>
+              </form>
+            )}
+          </Formik>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
- 
+
 export default ProjectsForm;
