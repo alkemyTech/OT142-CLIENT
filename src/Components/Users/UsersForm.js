fec66d7c975dc1967072709c8c8bf5d5991579ee
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-02-19 01:59:56 -0300

    Updating logic form

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,50 +1,118 @@
-import React, { useState } from 'react';
-import { Formik, Form, Field } from 'formik';
+import React, { useState, useEffect } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import * as Yup from 'yup';
 import '../FormStyles.css';
 
 const SignupSchema = Yup.object().shape({
-    name: Yup.string().email('Invalid email').required('Required'),
-    email: Yup.string().email('Invalid email').required('Required'),
-    password:Yup.string().email('Invalid email').required('Required'),
-    roleId: Yup.string().email('Invalid email').required('Required')
+    name: Yup.string().required('El campo Name no puede estar vacío').min(4, 'El campo name debe tener al menos 4 caracteres').max(10, 'El campo name puede tener máximo 10 caracteres'),
+    email: Yup.string().email('Email inválido').required('El campo Email no puede estar vacío'),
+    password:Yup.string().required('El campo Password no puede estar vacío').min(8, 'El campo password debe tener al menos 8 caracteres').max(10, 'El campo password puede tener máximo 15 caracteres')
   });
 
-const UserForm = () => {
-    const [initialValues, setInitialValues] = useState({
+ 
+  let userData = null
+   /*
+   //Testing
+   let userData = {
+      name: 'joel',
+      email: 'joel@example.com',
+      password: 'password',
+      roleId: 'user'
+  }*/
+//userDat = userData
+const UserForm = (userDat) => {
+    const [data, setData] = useState({
         name: '',
         email: '',
         password: '',
         roleId: ''
-    })
+        })
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        }
-    }
+        useEffect(() => {
+            if (userData === null) {
+                Swal.fire('Usuario inexistente, completar el formulario para crear un nuevo usuario');
+                } else {
+                   Swal.fire('Usuario existente, completar el formulario para actualizar el usuario');
+                   setData(userData);
+                }
+        }, [data])
+        
+    /*if (userData === null) {
+        axios.post('/user', {data})
+        .then((response) => {
+            Swal.fire('Usuario inexistente, complete el formulario para crear un nuevo usuario');
+        }).catch((error) => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
+        })
+    } else {
+    
+      /*axios.patch('/user/:id', { 
+            userData
+        }).then((response) => {
+            Swal.fire('Usuario existente, completar el formulario para actualizar el usuario');
+            setData(userData);
+        }).catch((error) => {
 
+        })
+    }*/
+    const initialValues = data;
+    console.log('initialValues', initialValues)
+    
     return (
-        <Formik>
-            <form className="form-container" onSubmit={handleSubmit}>
-                <input className="input-field" type="file"></input>
-                <input className="input-field" type="text" name="name" value={initialValues.name || ''} onChange={handleChange} placeholder="Name"></input>
-                <input className="input-field" type="text" name="email" value={initialValues.email || ''} onChange={handleChange} placeholder="Email"></input>
-                <input className="input-field" type="password" name="password" value={initialValues.password || ''} onChange={handleChange} placeholder="Password"></input>
-                <select className="input-field" value={initialValues.roleId || ''} onChange={e => setInitialValues({...initialValues, roleId: e.target.value})}>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={SignupSchema}
+            onSubmit={(values, { resetForm })=>{
+                resetForm();
+            }}
+        >
+            {({ values, handleSubmit, handleChange, handleBlur }) => (
+            <Form className="form-container" onSubmit={handleSubmit}>
+                <input 
+                    className="input-field" 
+                    type="file">
+                </input>
+                <Field 
+                    className="input-field" 
+                    autoComplete="off"
+                    type="text" 
+                    name="name" 
+                    value={userData === null ? values.name : initialValues.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur} 
+                    placeholder="Name">
+                </Field>
+                <ErrorMessage name='name' />
+                <Field 
+                    className="input-field" 
+                    autoComplete="off"
+                    type="text" 
+                    name="email" 
+                    value={userData === null ? values.email : initialValues.email} 
+                    onChange={handleChange} 
+                    placeholder="Email">
+                </Field>
+                <ErrorMessage name='email' />
+                <Field 
+                    className="input-field"
+                    type="password"
+                    autoComplete="off"
+                    name="password" 
+                    value={userData === null ? values.password : initialValues.password}
+                    onChange={handleChange} 
+                    placeholder="Password">
+                </Field>
+                <ErrorMessage name='password' />
+                <select className="input-field" value={data.roleId || ''} onChange={e => setData({...data.roleId, roleId: e.target.value})}>
                     <option value="" disabled >Select the role</option>
                     <option value="1">Admin</option>
                     <option value="2">User</option>
                 </select>
                 <button className="submit-btn" type="submit">Send</button>
-            </form>
+            </Form>
+            )}
         </Formik>
     );
 }
