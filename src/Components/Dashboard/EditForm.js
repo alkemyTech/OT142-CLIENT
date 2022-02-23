e5505bad23b5efc4a08ab433ab9bf5f13471ac23
ncaruso <nico.caruso1992@outlook.com>
2022-02-23 10:53:35 -0300

    cambios de estilos

--- src/Components/Dashboard/EditForm.js
+++ src/Components/Dashboard/EditForm.js
@@ -7,7 +7,10 @@ import {
   FormControl,
   FormLabel,
   FormErrorMessage,
-  Container,
+  Box,
+  Heading,
+  Stack,
+  Textarea,
   Flex,
 } from "@chakra-ui/react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
@@ -21,14 +24,9 @@ const initialValues = {
   socialLinks: "",
 };
 
-const onSubmit = (values) => {
-  
-};
+const onSubmit = (values) => {};
 
-const FORMATS = [
-  "application/png",
-  "application/jpg"
-];
+const FORMATS = ["application/png", "application/jpg"];
 
 const validationSchema = Yup.object({
   name: Yup.string()
@@ -37,8 +35,9 @@ const validationSchema = Yup.object({
 
   logo: Yup.mixed()
     .required("Please upload a logo")
-    .test('fileFormat', '.png o .jpg only', (value) => {
-      console.log("value dfile yup ", value); return value && FORMATS.includes(value.type);
+    .test("fileFormat", ".png o .jpg only", (value) => {
+      console.log("value dfile yup ", value);
+      return value && FORMATS.includes(value.type);
     }),
   shortDescription: Yup.string()
     .required("Please enter a short description")
@@ -62,105 +61,143 @@ const EditForm = () => {
   });
 
   return (
-    <Container>
-      <form className="form-container" onSubmit={formik.handleSubmit}>
-        <FormControl isInvalid={formik.errors.name && formik.touched.name}>
-          <FormLabel htmlFor="email">Name</FormLabel>
-          <Input
-            variant="outline"
-            type="text"
-            name="name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Enter your name"
-          ></Input>
+    <Flex
+      bg="secondary.background"
+      minHeight="100%"
+      width="100%"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <Box
+        width={{ base: "90%", md: "400px" }}
+        bg="secondary.card"
+        rounded="lg"
+        p={5}
+      >
+        <Heading marginBottom="1.5rem">Edición</Heading>
+        <form className="form-container" onSubmit={formik.handleSubmit}>
+          <Stack spacing={4} marginBottom="1rem">
+            <FormControl isInvalid={formik.errors.name && formik.touched.name}>
+              <FormLabel htmlFor="email">Nombre</FormLabel>
+              <Input
+                variant="outline"
+                type="text"
+                name="name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Ingrese nuevo nombre"
+              ></Input>
 
-          <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={formik.errors.logo && formik.touched.logo}>
-          <FormLabel htmlFor="logo">Logo</FormLabel>
-          <Input
-            // variant="outline"
-            type="file"
-            name="logo"
-            // value={formik.values.logo}
-            onChange={(event, editor) => {
-              const file = event.target.files;                                   
-              formik.setFieldValue('logo', file);
-              // console.log({ event, editor });
-            }}
-            
-          ></Input>
+              <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={formik.errors.logo && formik.touched.logo}>
+              <Stack justifyContent="space-between" isInline>
+                <FormLabel htmlFor="logo">Logo</FormLabel>
+              </Stack>
+              
+              <Input
+                // variant="outline"
+                type="file"
+                name="logo"
+                // value={formik.values.logo}
+                onChange={(event, editor) => {
+                  const file = event.target.files;
+                  formik.setFieldValue("logo", file);
+                  // console.log({ event, editor });
+                }}
+              ></Input>
 
-          <FormErrorMessage>{formik.errors.logo}</FormErrorMessage>
-        </FormControl>
-        <FormControl
-          isInvalid={
-            formik.errors.shortDescription && formik.touched.shortDescription
-          }
-        >
-          <FormLabel htmlFor="shortDescription">Short description</FormLabel>
-          <CKEditor
-            config={{name: "shortDescription"}}
-            editor={ClassicEditor}
-            data={formik.values.shortDescription}
-            name="shortDescription"
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              formik.setFieldValue("shortDescription", data);
-              console.log({ event, editor, data });
-            }}
-            onBlur={(event, editor) => {
-              const data = editor.getData();
-              formik.setFieldValue("shortDescription", data);
-              console.log("Blur.", editor);
-            }}
-          />
+              <FormErrorMessage>{formik.errors.logo}</FormErrorMessage>
+            </FormControl>
+            <FormControl
+              isInvalid={
+                formik.errors.shortDescription &&
+                formik.touched.shortDescription
+              }
+            >
+              <Stack justifyContent="space-between" isInline>
+                <FormLabel htmlFor="shortDescription">
+                  Descripción corta
+                </FormLabel>
+              </Stack>
+             
+              <CKEditor
+                config={{ name: "shortDescription", placeholder: "Ingrese nueva descripción corta" }}
+                editor={ClassicEditor}
+                data={formik.values.shortDescription}
+                name="shortDescription"
+                onChange={(event, editor) => {
+                  const data = editor.getData();
+                  formik.setFieldValue("shortDescription", data);
+                  console.log({ event, editor, data });
+                }}
+                onBlur={(event, editor) => {
+                  const data = editor.getData();
+                  formik.setFieldValue("shortDescription", data);
+                  console.log("Blur.", editor);
+                }}
+              />
 
-          <FormErrorMessage>{formik.errors.shortDescription}</FormErrorMessage>
-        </FormControl>
-        <FormControl
-          isInvalid={
-            formik.errors.longDescription && formik.touched.longDescription
-          }
-        >
-          <FormLabel htmlFor="longDescription">Long description</FormLabel>
-          <Input
-            variant="outline"
-            type="text"
-            name="longDescription"
-            value={formik.values.longDescription}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Write a long description"
-          ></Input>
+              <FormErrorMessage>
+                {formik.errors.shortDescription}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl
+              isInvalid={
+                formik.errors.longDescription && formik.touched.longDescription
+              }
+            >
+              <Stack justifyContent="space-between" isInline>
+                <FormLabel htmlFor="longDescription">Descripción larga</FormLabel>
+              </Stack>
+              
+              <Textarea
+                variant="outline"
+                type="text"
+                name="longDescription"
+                value={formik.values.longDescription}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Ingrese una nueva descripción"
+              ></Textarea>
 
-          <FormErrorMessage>{formik.errors.longDescription}</FormErrorMessage>
-        </FormControl>
-        <FormControl
-          isInvalid={formik.errors.socialLinks && formik.touched.socialLinks}
-        >
-          <FormLabel htmlFor="longDescription">Social links</FormLabel>
-          <Input
-            variant="outline"
-            type="text"
-            name="socialLinks"
-            value={formik.values.socialLinks}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Enter a social link"
-          ></Input>
+              <FormErrorMessage>
+                {formik.errors.longDescription}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl
+              isInvalid={
+                formik.errors.socialLinks && formik.touched.socialLinks
+              }
+            >
+              <Stack justifyContent="space-between" isInline>
+                <FormLabel htmlFor="longDescription">Redes sociales</FormLabel>
+              </Stack>
+              
+              <Input
+                variant="outline"
+                type="text"
+                name="socialLinks"
+                value={formik.values.socialLinks}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Ingrese una red social"
+              ></Input>
 
-          <FormErrorMessage>{formik.errors.socialLinks}</FormErrorMessage>
-        </FormControl>
-        <Flex mt="4">
-          <Button type="submit" size="md" variant="solid" colorScheme="teal">
-            Edit
-          </Button>
-        </Flex>
-      </form>
-    </Container>
+              <FormErrorMessage>{formik.errors.socialLinks}</FormErrorMessage>
+            </FormControl>
+          </Stack>
+
+          <Stack marginBottom="1rem">
+            <Button type="submit" size="md" variant="solid" colorScheme="teal">
+              Editar
+            </Button>
+          </Stack>
+        </form>
+      </Box>
+    </Flex>
   );
 };
 
