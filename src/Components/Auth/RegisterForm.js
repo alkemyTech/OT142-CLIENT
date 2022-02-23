91753effac5f65a68e70e1ac32053c3aab5b9598
ncaruso <nico.caruso1992@outlook.com>
2022-02-23 00:03:45 -0300

    mejoras en layout del formulario

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -7,8 +7,11 @@ import {
   FormControl,
   FormLabel,
   FormErrorMessage,
-  Container,
+  Box,
   Flex,
+  Heading,
+  Stack,
+  InputGroup
 } from "@chakra-ui/react";
 
 const initialValues = {
@@ -26,10 +29,8 @@ const onSubmit = (values) => {
 };
 
 const validationSchema = Yup.object({
-  name: Yup.string()
-    .required("Please enter your name"),
-  lastName: Yup.string()
-    .required("Please enter your lastname"),
+  name: Yup.string().required("Please enter your name"),
+  lastName: Yup.string().required("Please enter your lastname"),
   email: Yup.string()
     .email("Invalid email format")
     .required("Please enter your email"),
@@ -54,93 +55,143 @@ const RegisterForm = () => {
   });
 
   return (
-    <Container>
-      
-      <form className="form-container" onSubmit={formik.handleSubmit}>
-      <FormControl mb="4" isInvalid={formik.errors.name && formik.touched.name}>
-          <FormLabel htmlFor="name">Name</FormLabel>
-          <Input
-            variant="outline"
-            type="text"
-            name="name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Enter your name"
-          ></Input>
+    <Flex
+      bg="secondary.background"
+      minHeight="100%"
+      width="100%"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <Box
+        width={{ base: "90%", md: "400px" }}
+        bg="secondary.card"
+        rounded="lg"
+        p={5}
+      >
+        <Heading marginBottom="1.5rem">Registro</Heading>
+        <form className="form-container" onSubmit={formik.handleSubmit}>
+          <Stack spacing={4} marginBottom="1rem">
+            <FormControl
+              
+              isInvalid={formik.errors.name && formik.touched.name}
+            >
+              <FormLabel htmlFor="name">Nombre</FormLabel>
+              
+                <Input
+                  focusBorderColor="#D69E2E"
+                  variant="outline"
+                  type="text"
+                  name="name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  placeholder="Ingresa tu nombre"
+                ></Input>
 
-          <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
-        </FormControl>
-        <FormControl mb="4" isInvalid={formik.errors.lastName && formik.touched.lastName}>
-          <FormLabel htmlFor="lastName">Lastname</FormLabel>
-          <Input
-            variant="outline"
-            type="text"
-            name="lastName"
-            value={formik.values.lastName}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Enter your lastname"
-          ></Input>
+                <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
+              
+            </FormControl>
+            <FormControl
+              
+              isInvalid={formik.errors.lastName && formik.touched.lastName}
+            >
+              <Stack justifyContent="space-between" isInline>
+                <FormLabel htmlFor="lastName">Apellido</FormLabel>
+              </Stack>
+              
+              <Input
+                focusBorderColor="#D69E2E"
+                variant="outline"
+                type="text"
+                name="lastName"
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Ingresa tu apellido"
+              ></Input>
 
-          <FormErrorMessage>{formik.errors.lastName}</FormErrorMessage>
-        </FormControl>
-        <FormControl mb="4" isInvalid={formik.errors.email && formik.touched.email}>
-          <FormLabel htmlFor="email">Email address</FormLabel>
-          <Input
-            variant="outline"
-            type="email"
-            name="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Enter email"
-          ></Input>
+              <FormErrorMessage>{formik.errors.lastName}</FormErrorMessage>
+            </FormControl>
+            <FormControl
+              
+              isInvalid={formik.errors.email && formik.touched.email}
+            >
+              <Stack justifyContent="space-between" isInline>
+                <FormLabel htmlFor="email">Email</FormLabel>
+              </Stack>
+             
+              <Input
+                focusBorderColor="#D69E2E"
+                variant="outline"
+                type="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="name@example.com"
+              ></Input>
 
-          <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-        </FormControl>
-        <FormControl mb="4"
-          isInvalid={formik.errors.password && formik.touched.password}
-        >
-          <FormLabel htmlFor="password">Password</FormLabel>
-          <Input
-            variant="outline"
-            type="password"
-            name="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Enter your password"
-          ></Input>
+              <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+            </FormControl>
+            <FormControl
+              
+              isInvalid={formik.errors.password && formik.touched.password}
+            >
+              <Stack justifyContent="space-between" isInline>
+                <FormLabel htmlFor="password">Contraseña</FormLabel>
+              </Stack>
+              
+              <Input
+                focusBorderColor="#D69E2E"
+                variant="outline"
+                type="password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Ingresa tu contraseña"
+              ></Input>
 
-          <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-        </FormControl>
-        <FormControl mb="4"
-          isInvalid={
-            formik.errors.passwordRepeat && formik.touched.passwordRepeat
-          }
-        >
-          <FormLabel htmlFor="passwordRepeated">Repeat your password</FormLabel>
-          <Input
-            variant="outline"
-            type="password"
-            name="passwordRepeat"
-            value={formik.values.passwordRepeat}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            placeholder="Repeat your password"
-          ></Input>
+              <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+            </FormControl>
+            <FormControl
+              
+              isInvalid={
+                formik.errors.passwordRepeat && formik.touched.passwordRepeat
+              }
+            >
+              <Stack justifyContent="space-between" isInline>
+                <FormLabel htmlFor="passwordRepeated">
+                  Confirma tu contraseña
+                </FormLabel>
+              </Stack>
+              
+              <Input
+                focusBorderColor="#D69E2E"
+                variant="outline"
+                type="password"
+                name="passwordRepeat"
+                value={formik.values.passwordRepeat}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                placeholder="Confirma tu contraseña"
+              ></Input>
 
-          <FormErrorMessage>{formik.errors.passwordRepeat}</FormErrorMessage>
-        </FormControl>
-        <Flex mt="4">
-          <Button type="submit" size="md" variant="solid" colorScheme="teal">
-            Register
-          </Button>
-        </Flex>
-      </form>
-      
-    </Container>
+              <FormErrorMessage>
+                {formik.errors.passwordRepeat}
+              </FormErrorMessage>
+            </FormControl>
+          </Stack>
+
+          <Stack marginBottom="1rem">
+            <Button type="submit" size="md" variant="solid" colorScheme="teal">
+              Registrar
+            </Button>
+          </Stack>
+        </form>
+      </Box>
+    </Flex>
   );
 };
 
