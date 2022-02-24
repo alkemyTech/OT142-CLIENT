962d3a1eb33743c8daa572e4f1e570cddabb1247
Mauro Erben <mauroerben100@hotmail.com>
2022-02-24 19:16:26 -0300

    delete request added in privateApiService

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
+const BASE_URL = "http://ongapi.alkemy.org/api";
+
 const config = {
   headers: {
-    Group: 01, //Aqui va el ID del equipo!!
+    // Group: 01, //Aqui va el ID del equipo!!
+    Authorization: localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : null
   },
 };
 
-const Get = () => {
-  axios
-    .get("https://jsonplaceholder.typicode.com/users", config)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-};
-
-export default Get;
+const Delete = (route, id) => {
+  return axios.delete(`${BASE_URL}/${route}/${id}`, config)
+    .then(res => res.data)
+    .catch(error => console.log(error));
+}
+export default Delete;
