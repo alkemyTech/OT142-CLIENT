2e5e4a91ccd0893d678ebfd897dcd99bf98fa03f
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-02-25 00:05:42 -0300

    adding public put request service

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,14 +2,32 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 142                
     }
 }
+
+//Setting default global config for all request:
+
+axios.defaults.headers.common['Group'] = 142
 
 const Get = () => {
     axios.get('https://jsonplaceholder.typicode.com/users', config)
     .then(res => console.log(res))
     .catch(err => console.log(err))
 }
+
+export function updatePublicServer(url, id, body) {
+    axios
+      .put(`${url}/${id}`, {
+        body
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
 
 export default Get
