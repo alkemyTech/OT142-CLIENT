2de1981afd3d9bcb168d02b4a1c29d96b8381f38
Agustin <agus.ammazzagatti@gmail.com>
2022-02-25 10:55:18 -0300

    NewsList Ended

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
 import CategoriesForm from './Components/Categories/CategoriesForm';
 import NewsForm from './Components/News/NewsForm';
+import NewsList from './Components/News/NewListTable/NewsList';
 import SlidesForm from './Components/Slides/SlidesForm';
 import TestimonialForm from './Components/Testimonials/TestimonialsForm';
 import UserForm from './Components/Users/UsersForm';
@@ -37,6 +38,7 @@ function App() {
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/backoffice/organization/edit" component={EditForm} />
           <Route path="/backoffice/members/edit" component={MembersEdit} />
+          <Route path="/backoffice/news" component={NewsList} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
