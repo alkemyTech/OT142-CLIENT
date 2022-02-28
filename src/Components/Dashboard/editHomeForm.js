10d6038ac41968e9b5b1afc69d8a2b1e93ef2a6a
gabibenavidez <gabriel.m.benavidez@gmail.com>
2022-02-28 15:00:34 -0300

    merge conflicts solved

--- src/Components/Dashboard/editHomeForm.js
+++ src/Components/Dashboard/editHomeForm.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from 'yup';
 
+
 const EditHomeForm = () => {
     const [editData, setEditData] = useState({});
     const [sendedForm, setSendedForm] = useState(false);
@@ -39,7 +40,7 @@ const EditHomeForm = () => {
                 }}
             >
             {( {errors} ) => (
-                <Form> 
+                <Form className="formulario"> 
                     <div>
                         <label htmlFor="welcomeText">Texto de bienvenida</label>
                         <Field 
