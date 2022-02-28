f4a24c6727a418452503d0cce74a564751a3b84a
[nico godoy] <[nicogodoycvu10@gmail.com]>
2022-02-27 22:17:42 -0300

    HTTP base para las peticiones de Categorias

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,62 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+
 import "../FormStyles.css";
-import axios from "axios";
+import {
+  postRequest,
+  patchRequest,
+  getRequest,
+  GetCategie,
+} from "../../Services/ServiceCtegories";
 import {
   Image,
   Input,
   Button,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
   Stack,
-  Textarea,
   Heading,
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
 } from "@chakra-ui/react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import styleCS from './styleCS.css'
-
-const state = {
-  name: "",
-  description: "",
-  image: "",
-};
+import styleCS from "./styleCS.css";
+import { useParams } from "react-router-dom";
 
 const CategoriesForm = () => {
-  const { categorie } = useParams();
-  const [initialValues, setInitialValues] = useState(state==false);
+  const [initialValues, setInitialValues] = useState({
+    name: "",
+    description: "",
+   
+  });
 
+  const data = useParams();
+  console.log(data.id);
 
   useEffect(() => {
-  
-    if (categorie) {
-      var BASE_URL = `http://ongapi.alkemy.org/api/categories/${categorie}`;
-      axios
-        .get(BASE_URL)
-        .then((res) => res.data.data)
-        .then((data) =>{
-            setInitialValues({
-                      name: data.name,
-                      description: data.description,
-                      image: data.image,
-                    })
-        
-        }
-        
-        
-          
-        );
-        
+    if (data.id) {
+      getRequest(`categories/${data.id}`)
+     .then((data) => {
+       return data.data;
+     }).then((data)=>{
+       console.log(data.data)
+       setInitialValues({
+         name:data.data.name,
+         description:data.data.description,
+         image:data.data.image
+       })
+     })
     }
+  }, []);
 
-   
-  },[categorie]);
+
   const handleChange = (e) => {
     if (e.target.name === "name") {
       setInitialValues({ ...initialValues, name: e.target.value });
@@ -66,21 +57,27 @@ const CategoriesForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("enviado ", initialValues);
-    if (categorie) {
-      const res = await axios.patch("/categories", initialValues);
-      return res;
+    if (data.id) {
+      patchRequest(`categories/${data.id}`,initialValues).then((data) => {
+       console.log(data)
+        
+      });
     } else {
-      const res = await axios.post("/categories", initialValues);
-      return res;
+      postRequest("/categories",initialValues).then((data) => {
+        console.log(data)
+        
+      });
     }
   };
 
   return (
     <>
-        <form className="global" onSubmit={handleSubmit}>
-          <Stack>
-            <Heading>Edit/Categorie</Heading>
-          </Stack>
+      <form className="global" onSubmit={handleSubmit}>
+        <Stack>
+          <Heading>Edit/Categorie</Heading>
+        </Stack>
+
+        <Stack>
           <Stack>
             <FormControl isRequired>
               <FormLabel htmlFor="first-name">Name</FormLabel>
@@ -94,12 +91,15 @@ const CategoriesForm = () => {
                 value={initialValues.name}
                 onChange={handleChange}
                 placeholder="Name"
-              ></Input>
+              >
+               
+              </Input>
             </FormControl>
           </Stack>
           <Stack spacing={4}>
             <FormControl isRequired>
               <FormLabel htmlFor="first-name">Description</FormLabel>
+             
               <CKEditor
                 config={{ placeholder: "...Description" }}
                 editor={ClassicEditor}
@@ -107,7 +107,10 @@ const CategoriesForm = () => {
                 onChange={(event, editor) => {
                   const data = editor.getData();
                   console.log(data);
-                  setInitialValues({ ...initialValues, description: data });
+                  setInitialValues({
+                    ...initialValues,
+                    description: data,
+                  });
                 }}
               />
             </FormControl>
@@ -123,25 +126,232 @@ const CategoriesForm = () => {
                 onChange={(event) => {
                   const files = event.target.files;
                   let myFiles = Array.from(files);
-                  setInitialValues({
-                    ...initialValues,
-                    image: myFiles[0].name,
-                  });
+                  // setInitialValues({
+                  //   ...initialValues,
+                  //   image: myFiles[0].name,
+                  // });
                 }}
                 mb={2}
               />
-              <Image id="img-preview"></Image>
+              <Image src={initialValues.image} id="img-preview"></Image>
             </FormControl>
-          </Stack>
+          </Stack> 
+
           <Stack spacing={4}>
             <Button mt={4} colorScheme="teal" type="submit">
               Send
             </Button>
           </Stack>
-        </form>
-      
+        </Stack>
+      </form>
     </>
   );
 };
 
 export default CategoriesForm;
+
+{
+  /* <>
+      <form className="global" onSubmit={handleSubmit}>
+        <Stack>
+          <Heading>Edit/Categorie</Heading>
+        </Stack>
+
+        <Stack>
+          <FormControl isRequired>
+            <FormLabel htmlFor="first-name">Name</FormLabel>
+            <Input
+              isInvalid
+              focusBorderColor="red.300"
+              variant="filled"
+              type="text"
+              minLength={4}
+              name="name"
+              value={initialValues.name}
+              onChange={handleChange}
+              placeholder="Name"
+            ></Input>
+          </FormControl>
+        </Stack>
+        <Stack spacing={4}>
+          <FormControl isRequired>
+            <FormLabel htmlFor="first-name">Description</FormLabel>
+            <CKEditor
+              config={{ placeholder: "...Description" }}
+              editor={ClassicEditor}
+              data={initialValues.description}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                console.log(data);
+                setInitialValues({ ...initialValues, description: data });
+              }}
+            />
+          </FormControl>
+        </Stack>
+
+        <Stack spacing={4}>
+          <FormControl isRequired>
+            <FormLabel htmlFor="first-name">Image</FormLabel>
+            <Input
+              id="image"
+              type="file"
+              variant="flushed"
+              onChange={(event) => {
+                const files = event.target.files;
+                let myFiles = Array.from(files);
+                setInitialValues({
+                  ...initialValues,
+                  image: myFiles[0].name,
+                });
+              }}
+              mb={2}
+            />
+            <Image id="img-preview"></Image>
+          </FormControl>
+        </Stack>
+
+        <Stack spacing={4}>
+          <Button mt={4} colorScheme="teal" type="submit">
+            Send
+          </Button>
+        </Stack>
+      </form>
+    </> */
+}
+
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import "../FormStyles.css";
+// import axios from "axios";
+// import {
+//   Image,
+//   Input,
+//   Button,
+//   FormControl,
+//   FormLabel,
+//   FormErrorMessage,
+//   FormHelperText,
+//   Stack,
+//   Textarea,
+//   Heading,
+//   Alert,
+//   AlertIcon,
+//   AlertTitle,
+//   AlertDescription,
+// } from "@chakra-ui/react";
+// import { CKEditor } from "@ckeditor/ckeditor5-react";
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+// import styleCS from "./styleCS.css";
+
+// const state = {
+//   name: "",
+//   description: "",
+//   image: "",
+// };
+
+// const CategoriesForm = () => {
+//   const { categorie } = useParams();
+//   const [initialValues, setInitialValues] = useState(state == false);
+
+//   useEffect(() => {
+//     if (categorie) {
+//       var BASE_URL = `http://ongapi.alkemy.org/api/categories/${categorie}`;
+//       axios
+//         .get(BASE_URL)
+//         .then((res) => res.data.data)
+//         .then((data) => {
+//           setInitialValues({
+//             name: data.name,
+//             description: data.description,
+//             image: data.image,
+//           });
+//         });
+//     }
+//   }, [categorie]);
+//   const handleChange = (e) => {
+//     if (e.target.name === "name") {
+//       setInitialValues({ ...initialValues, name: e.target.value });
+//     }
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     console.log("enviado ", initialValues);
+//     if (categorie) {
+//       const res = await axios.patch("/categories", initialValues);
+//       return res;
+//     } else {
+//       const res = await axios.post("/categories", initialValues);
+//       return res;
+//     }
+//   };
+
+//   return (
+//     <>
+//       <form className="global" onSubmit={handleSubmit}>
+//         <Stack>
+//           <Heading>Edit/Categorie</Heading>
+//         </Stack>
+//         <Stack>
+//           <FormControl isRequired>
+//             <FormLabel htmlFor="first-name">Name</FormLabel>
+//             <Input
+//               isInvalid
+//               focusBorderColor="red.300"
+//               variant="filled"
+//               type="text"
+//               minLength={4}
+//               name="name"
+//               value={initialValues.name}
+//               onChange={handleChange}
+//               placeholder="Name"
+//             ></Input>
+//           </FormControl>
+//         </Stack>
+//         <Stack spacing={4}>
+//           <FormControl isRequired>
+//             <FormLabel htmlFor="first-name">Description</FormLabel>
+//             <CKEditor
+//               config={{ placeholder: "...Description" }}
+//               editor={ClassicEditor}
+//               data={initialValues.description}
+//               onChange={(event, editor) => {
+//                 const data = editor.getData();
+//                 console.log(data);
+//                 setInitialValues({ ...initialValues, description: data });
+//               }}
+//             />
+//           </FormControl>
+//         </Stack>
+
+//         <Stack spacing={4}>
+//           <FormControl isRequired>
+//             <FormLabel htmlFor="first-name">Image</FormLabel>
+//             <Input
+//               id="image"
+//               type="file"
+//               variant="flushed"
+//               onChange={(event) => {
+//                 const files = event.target.files;
+//                 let myFiles = Array.from(files);
+//                 setInitialValues({
+//                   ...initialValues,
+//                   image: myFiles[0].name,
+//                 });
+//               }}
+//               mb={2}
+//             />
+//             <Image id="img-preview"></Image>
+//           </FormControl>
+//         </Stack>
+//         <Stack spacing={4}>
+//           <Button mt={4} colorScheme="teal" type="submit">
+//             Send
+//           </Button>
+//         </Stack>
+//       </form>
+//     </>
+//   );
+// };
+
+// export default CategoriesForm;
