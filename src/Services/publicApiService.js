648cca5845b05c7134c0f768d394d1c5d7f505b6
maxirama <maximilianogastonrama@gmail.com>
2022-02-28 18:46:02 -0300

    Modified publicApIService. Add const axiosInstance. Add getRequest method.

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,15 +1,33 @@
-import axios from 'axios';
+import axios from "axios";
+import { API } from "../Activities/Detail/hooks/API";
 
 const config = {
-    headers: {
-        Group: ''               //Aqui va el ID del equipo!!
-    }
-}
-
+  headers: {
+    Group: "", //Aqui va el ID del equipo!!
+  },
+};
 
 export const API_SERVICE_POST = axios.create({
-    BASE_URL: 'http://ongapi.alkemy.org/public/api',
-    method: 'post',
-    url: '',
-    responseType: 'json',
+  BASE_URL: "http://ongapi.alkemy.org/public/api",
+  method: "post",
+  url: "",
+  responseType: "json",
 });
+
+export const axiosInstance = axios.create({
+  baseURL: "http://ongapi.alkemy.org/public/api",
+  responseType: "json",
+});
+
+export const getRequest = (path, id) => {
+  const route = id ? `${path}/${id}` : `${path}`;
+
+  return axiosInstance
+    .get(route)
+    .then((response) => {
+      return response.data.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
