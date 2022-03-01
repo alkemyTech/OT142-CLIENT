3d28c470b11de8be18a63002d0d1741ea6419403
[nico godoy] <[nicogodoycvu10@gmail.com]>
2022-03-01 12:34:33 -0300

    probando lista de contacto

--- src/App.js
+++ src/App.js
@@ -1,56 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import ActivitiesForm from './Components/Activities/ActivitiesForm';
-import CategoriesForm from './Components/Categories/CategoriesForm';
-import NewsForm from './Components/News/NewsForm';
-import SlidesForm from './Components/Slides/SlidesForm';
-import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-import UserForm from './Components/Users/UsersForm';
-import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
-import TableCategorie from './Components/Categories/TableCategorie';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ActivitiesForm from "./Components/Activities/ActivitiesForm";
+import CategoriesForm from "./Components/Categories/CategoriesForm";
+import NewsForm from "./Components/News/NewsForm";
+import SlidesForm from "./Components/Slides/SlidesForm";
+import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
+import UserForm from "./Components/Users/UsersForm";
+import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
+import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
+import MembersForm from "./Components/Members/MembersForm";
+import ProjectsForm from "./Components/Projects/ProjectsForm";
+import TableCategorie from "./Components/Categories/TableCategorie";
+import ContactDate from "./Components/Contact/ContactDate";
 import { ChakraProvider } from "@chakra-ui/react";
-
-
 
 function App() {
   return (
-    <> <ChakraProvider>
-      <BrowserRouter>
-        <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/create-category/:categorie?" component={CategoriesForm}/>
-          <Route path="/backoffice/categories" component={TableCategorie}/>
-         
-
-        
-          
-
-          
-          
-
-        </Switch>
-      </BrowserRouter>
+    <>
+      {" "}
+      <ChakraProvider>
+        <BrowserRouter>
+          <Switch>
+            {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+            <Route path="/create-activity" component={ActivitiesForm} />
+            <Route path="/create-news" component={NewsForm} />
+            <Route path="/backoffice/create-slide" component={SlidesForm} />
+            <Route path="/create-testimonials" component={TestimonialForm} />
+            <Route path="/create-user" component={UserForm} />
+            <Route path="/create-member" component={MembersForm} />
+            <Route path="/create-project" component={ProjectsForm} />
+            <Route path="/school-campaign" component={SchoolCampaign} />
+            <Route path="/toys-campaign" component={ToysCampaign} />
+            <Route path="/create-category/:categorie?"component={CategoriesForm}/>
+            <Route path="/backoffice/categories" component={TableCategorie} />
+            <Route path="/contacto" component={ContactDate}/>
+          </Switch>
+        </BrowserRouter>
       </ChakraProvider>
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header"></header>
+      </div>
     </>
   );
 }
---
+++ src/Components/Contact/Date.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ContactDate from './ContactDate'
+import TitleContact from "./TitleContact";
+import {AiFillFacebook} from "react-icons/ai";
+
+
+export default function Date(props) {
+   
+ console.log(props)   
+    
+  return (
+    <>
+     <TitleContact/>
+    <AiFillFacebook/>
+    </>
+  )
+}
