3477bc96ac677504184e01c1a409932f625d3977
maxirama <maximilianogastonrama@gmail.com>
2022-03-01 00:07:54 -0300

    Create Members component. Add link to backoffice/members/create

---
+++ src/Components/Members/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
+
+const Members = ({ members }) => {
+  return <div>Members</div>;
+};
+
+export default Members;
