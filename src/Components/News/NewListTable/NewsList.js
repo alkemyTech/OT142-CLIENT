a56b4589b9008d6a210f7e76134043e6e80eb9f1
ncaruso <nico.caruso1992@outlook.com>
2022-03-01 20:05:14 -0300

    conexion servicio de novedades  con la lista  de novedades

--- src/Components/News/NewListTable/NewsList.js
+++ src/Components/News/NewListTable/NewsList.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
-import NewListTable from './NewListTable';
+import React, { useState, useEffect } from 'react';
+import NewsListTable from './NewsListTable';
+import { getNews } from "../../../Services/newsService";
 
 const NewsList = () => {
-    const newsMock = [
-        { id: 1, name: 'Nombre de prueba 1', image: 'Imagen de prueba 1', createdAt: 'Descripcion de prueba 1' },
-        { id: 2, name: 'Nombre de prueba 2', image: 'Imagen de prueba 2', createdAt: 'Descripcion de prueba 2' },
-        { id: 3, name: 'Nombre de prueba 3', image: 'Imagen de prueba 3', createdAt: 'Descripcion de prueba 3' }
-    ];
+    // const newsMock = [
+    //     { id: 1, name: 'Nombre de prueba 1', image: 'Imagen de prueba 1', createdAt: 'Descripcion de prueba 1' },
+    //     { id: 2, name: 'Nombre de prueba 2', image: 'Imagen de prueba 2', createdAt: 'Descripcion de prueba 2' },
+    //     { id: 3, name: 'Nombre de prueba 3', image: 'Imagen de prueba 3', createdAt: 'Descripcion de prueba 3' }
+    // ];
 
-    const [data] = useState(newsMock);
+    const [data, setData] = useState([]);
+    useEffect(async () => {
+      const news = await getNews("news");
+      setData(news.data);
+      
+    }, [])
+    
 
     return (
         <>
             {data.length > 0 ?
                 <div >
-                    <NewListTable data={data} />
+                    <NewsListTable data={data} />
                 </div>
                 :
                 <p>No hay novedades</p>
