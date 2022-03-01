6de5762c4e3267c17051fd77f4c3dd833a235f82
maxirama <maximilianogastonrama@gmail.com>
2022-03-01 00:08:31 -0300

    Add Link to backoffice/members/create

--- src/Components/Members/index.js
+++ src/Components/Members/index.js
@@ -9,9 +9,22 @@ import {
   Td,
   TableCaption,
 } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+// Criterios de aceptación: Al ingresar a la ruta /backoffice/members,
+// mostrará el listado de Miembros de la ONG para el usuario administrador en una tabla.
+//El mismo tendrá datos mockeados para representar los datos, que posteriormente se obtendrán desde el endpoint de listado de Miembros.
+//La tabla mostará los campos name y photo,
+// y las acciones para eliminar y editar. En la sección superior, mostrará un componente <Link> que
+// redirigirá a la ruta /backoffice/members/create.
 
 const Members = ({ members }) => {
-  return <div>Members</div>;
+  return (
+    <>
+      <Link to="/backoffice/members/create">Add Members</Link>
+      <div>Members</div>
+    </>
+  );
 };
 
 export default Members;
