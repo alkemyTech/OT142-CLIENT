5e6b1071bae970762537a680576ef8f613db2ca3
Agustin <agus.ammazzagatti@gmail.com>
2022-03-02 20:21:43 -0300

    app js fixed

--- src/App.js
+++ src/App.js
@@ -17,23 +17,22 @@ import ProjectsForm from './Components/Projects/ProjectsForm';
 import ActivityDetail from './Activities/Detail/Components/ActivityDetail';
 import Activities from './Activities/Detail/Components/Activities';
 import Dashboard from "./Components/Dashboard/index.js";
-import OrganizationBoard from "./Components/Dashboard/OrganizationBoard.js";
+/* import OrganizationBoard from "./Components/Dashboard/OrganizationBoard.js"; */
 import { NewsDetail } from './Components/News/Detail/NewsDetail'
 import EditForm from "./Components/Dashboard/EditForm";
 import RegisterForm from "./Components/Auth/RegisterForm";
-import Contact from './Components/Contact';
+/* import Contact from './Components/Contact';
 import Footer from "./Components/Footer";
 import About from './Components/About';
 import Home from "./Components/Home";
 import MembersList from './Components/AboutUs/MembersList';
-import UserList from './Components/Dashboard/UsersList';
+import UserList from './Components/Dashboard/UsersList'; */
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-<<<<<<< HEAD
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
@@ -52,32 +51,7 @@ function App() {
           <Route path="/actividades/:id" component={ActivityDetail} />
           <Route path="/actividades" component={Activities} />
           <Route path="/register" component={RegisterForm} />
-          <Route path='/contact-form' component={ContactForm} />
-=======
-          <Route path="/" exact component={Home} />
-          <Route exact path="/create-activity" component={ActivitiesForm} />
-          <Route exact path="/create-category" component={CategoriesForm} />
-          <Route exact path="/Novedades/:id" component={NewsDetail} />
-          <Route exact path="/backoffice" component={Dashboard} />
-          <Route exact path="/backoffice/create-slide" component={SlidesForm} />
-          <Route exact path="/backoffice/organization/edit" component={EditForm} />
-          <Route exact path="/backoffice/organization" component={OrganizationBoard} />
-          <Route exact path="/backoffice/news/:id?" component={NewsForm} />
-          <Route exact path="/backoffice/users" component={UserList} />
-          <Route exact path="/create-testimonials" component={TestimonialForm} />
-          <Route exact path="/create-user" component={UserForm} />
-          <Route exact path="/create-member" component={MembersForm} />
-          <Route exact path="/create-project" component={ProjectsForm} />
-          <Route exact path="/update-project/:id" component={ProjectsForm} />
-          <Route exact path="/school-campaign" component={SchoolCampaign} />
-          <Route exact path="/toys-campaign" component={ToysCampaign} />
-          <Route exact path="/actividades/:id" component={ActivityDetail} />
-          <Route exact path="/actividades" component={Activities} />
-          <Route exact path="/register" component={RegisterForm} />
-          <Route exact path='/contact' component={Contact} />
-          <Route exact path='/nosotros' component={About} />
-          <Route exact path='/about-us/members' component={MembersList} />
->>>>>>> 869030e5b1502c811a5d1ff375c64e7a3c5739e8
+          {/*           <Route path='/contact-form' component={ContactForm} /> */}
         </Switch>
       </BrowserRouter>
     </>
