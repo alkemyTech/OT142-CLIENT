8486b6f3bd5befe0eccde2bc61d36c01d9bfbd6e
[nico godoy] <[nicogodoycvu10@gmail.com]>
2022-03-02 06:53:11 -0300

    petición PUT a los endpoints privados de la API

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -5,15 +5,18 @@ const BASE_URL = "http://ongapi.alkemy.org/api";
 const config = {
   headers: {
     // Group: 01, //Aqui va el ID del equipo!!
-    Authorization: localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : null
+    Authorization: localStorage.getItem("token")
+      ? `Bearer ${localStorage.getItem("token")}`
+      : null,
   },
 };
 
 export const Delete = (route, id) => {
-  return axios.delete(`${BASE_URL}/${route}/${id}`, config)
-    .then(res => res.data)
-    .catch(error => console.log(error));
-}
+  return axios
+    .delete(`${BASE_URL}/${route}/${id}`, config)
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
+};
 
 export const Get = () => {
   axios
@@ -24,13 +27,30 @@ export const Get = () => {
 
 // PATCH METHOD
 export const patchPrivate = async (endpoint, id, body) => {
-    try {  
-        const response = await axios.patch(`http://ongapi.alkemy.org/api/${endpoint}/${id}`,body,{
-            headers: tokenFunction(),
-        });   
-        return response;    
-    } catch (err) {
-        return err;
-    }
+  try {
+    const response = await axios.patch(
+      `http://ongapi.alkemy.org/api/${endpoint}/${id}`,
+      body,
+      {
+        headers: tokenFunction(),
+      }
+    );
+    return response;
+  } catch (err) {
+    return err;
+  }
 };
 
+export const PutPrivate = async (endpoint, body, id) => {
+  try {
+    const res = axios.put(
+      `http://ongapi.alkemy.org/api/${endpoint}/${id}`,
+      body,
+      config
+    );
+
+    return res;
+  } catch (err) {
+    return err;
+  }
+};
