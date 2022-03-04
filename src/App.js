ee5782ec65f3980cfe3f5281f00c01a54f2ae619
Agustin <agus.ammazzagatti@gmail.com>
2022-03-04 14:59:22 -0300

    resolving conflicts

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ActivitiesForm from "./Components/Activities/ActivitiesForm";
 import CategoriesForm from "./Components/Categories/CategoriesForm";
 import NewsForm from "./Components/News/NewsForm";
-import NewsList from "./Components/News/NewListTable/NewsList";
+/* import NewsList from "./Components/News/NewListTable/NewsList"; */
 import SlidesForm from "./Components/Slides/SlidesForm";
 import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
 import UserForm from "./Components/Users/UsersForm";
@@ -31,7 +31,8 @@ import UserList from './Components/Dashboard/UsersList'; */
 
 function App() {
   return (
-    <> <ChakraProvider>
+    <>
+      {/*      <ChakraProvider> */}
       <BrowserRouter>
         <Switch>
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
@@ -42,7 +43,7 @@ function App() {
           <Route path="/backoffice" component={Dashboard} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/backoffice/organization/edit" component={EditForm} />
-          <Route path="/backoffice/members/edit" component={MembersEdit} />
+          {/*  <Route path="/backoffice/members/edit" component={MembersEdit} /> */}
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
@@ -55,8 +56,9 @@ function App() {
           {/*           <Route path='/contact-form' component={ContactForm} /> */}
         </Switch >
       </BrowserRouter >
+      {/*   </ChakraProvider> */}
     </>
-      );
+  );
 }
 
-      export default App;
+export default App;
