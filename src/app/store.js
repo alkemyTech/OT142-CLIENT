4471456b3081e346deae56b5dca4e33cb80483f1
Gabriel Rea <gabriel.rea96@gmail.com>
2022-03-06 12:12:16 -0300

    Reducer creado con listado

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import ActivitiesReducer from '../Reducers/activitiesSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    ActivitiesReducer: ActivitiesReducer
   },
 });
