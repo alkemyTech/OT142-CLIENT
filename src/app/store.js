1fb47da94594a608d06772fa37220697e34623e1
Gabriel Rea <gabriel.rea96@gmail.com>
2022-03-06 18:25:32 -0300

    reducer utilizado en el backoffice de activities

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import ActivitiesReducer from '../Reducers/activitiesSlice';
+import activitiesReducer from '../Reducers/activitiesSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
-    ActivitiesReducer
+    activitiesReducer
   },
 });
