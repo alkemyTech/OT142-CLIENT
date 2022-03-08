d934647feafee16ea176f161fe04869450df4f7c
maxirama <maximilianogastonrama@gmail.com>
2022-03-08 20:38:20 -0300

    Add Spinner and Alert to src/Activities/Detail/Components/ ActivityDetail.js

--- src/Activities/Detail/Components/ActivityDetail.js
+++ src/Activities/Detail/Components/ActivityDetail.js
@@ -1,45 +1,69 @@
 import { useEffect, useState, useCallback } from "react";
-import { Container, Text, Image, Spinner } from '@chakra-ui/react';
+import { Container, Text, Image, Spinner, Center } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { get } from "../../../Services/publicApiService";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
 
 const ActivityDetail = () => {
-    const { id } = useParams();
-    const [activity, setActivity] = useState();
-    const [error, setError] = useState();
-    console.log(id, activity);
+  const { id } = useParams();
+  const [activity, setActivity] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
 
-    const getData = useCallback(async () => {
-        try {
-            const { data } = await get(`/activities/${id}`)
-            setActivity(data.data);
-        } catch (e) {
-            setError(true);
-        }
-    }, [])
+  const getData = useCallback(async () => {
+    try {
+      const { data } = await get(`/activities/${id}`);
+      setActivity(data.data);
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      setError(true);
+    }
+  }, []);
 
-    useEffect(() => {
-        console.log("prueba");
-        getData();
-    }, [getData]);
+  useEffect(() => {
+    getData();
+  }, [getData]);
 
+  return (
+    <>
+      {loading === true && (
+        <Center>
+          {" "}
+          <Spinner mt="1em" d="flex" size="xl" />
+        </Center>
+      )}
 
-    return (
-        <>
-            {activity ?
-                <Container d='flex' justifyContent='center' alignItems='center' flexDirection="column">
-                    <Text as="h1">
-                        {activity.name}
-                    </Text>
-                    <Image boxSize={200} src={activity.image} />
-                    <Text as="h3" align='center'>
-                        {activity.description}
-                    </Text>
-                </Container>
-                : <Spinner d="flex" size="xl" />}
-
-        </>
-    )
-}
+      {error === false ? (
+        <Container
+          d="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Text as="h1">{activity.name}</Text>
+          <Image boxSize={200} src={activity.image} />
+          <Text as="h3" align="center">
+            {activity.description}
+          </Text>
+        </Container>
+      ) : (
+        <Center>
+          {loading === false && (
+            <Alert mt="1em" p="1em" status="error" width="auto">
+              <AlertIcon />
+              <AlertTitle mr={2}> Ups! Algo salió mal.</AlertTitle>
+            </Alert>
+          )}
+        </Center>
+      )}
+    </>
+  );
+};
 
 export default ActivityDetail;
