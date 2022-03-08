9b2cf1e0f5a719e12037e51ad361454882c962fc
joPorpatto <joel.porpatto@gmail.com>
2022-03-07 23:21:24 -0300

    Merge main

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,42 +1,28 @@
 import axios from "axios";
 
-const BASE_URL = "http://ongapi.alkemy.org/api";
-
-const config = {
+export const axiosInstance = axios.create({
+  baseURL: "http://ongapi.alkemy.org/api",
   headers: {
-    // Group: 01, //Aqui va el ID del equipo!!
+    Group: "142",
   },
+});
+
+export const get = (path) => {
+  return axiosInstance.get(path);
 };
 
-export const getAuthorizationToken = () => {
-  const auth = {
-    Authorization: localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : null
-  }
-  return auth;
-}
-
-export const Delete = (route, id) => {
-  return axios.delete(`${BASE_URL}/${route}/${id}`, config)
-    .then(res => res.data)
-    .catch(error => console.log(error));
-}
-
-export const Get = () => {
-  axios
-    .get("https://jsonplaceholder.typicode.com/users", config)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+export const getById = (path, id) => {
+  return axiosInstance.get(`${path}/${id}`);
 };
 
-// PATCH METHOD
-export const patchPrivate = async (endpoint, id, body) => {
-  try {
-    const response = await axios.patch(`http://ongapi.alkemy.org/api/${endpoint}/${id}`, body, {
-      headers: tokenFunction(),
-    });
-    return response;
-  } catch (err) {
-    return err;
-  }
+export const put = (path, id, body) => {
+  return axiosInstance.put(`${path}/${id}`, body);
 };
 
+export const post = (path, body) => {
+  return axiosInstance.post(path, body);
+};
+
+export const remove = (path, id) => {
+  return axiosInstance.delete(`${path}/${id}`);
+};
