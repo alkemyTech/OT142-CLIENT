75b7bf7cd2bd294515176750846b6d3e2b5362f3
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-03-09 15:07:43 -0300

    correcting and cleaning code

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = "http://ongapi.alkemy.org/api";
+const BASE_URL = "https://ongapi.alkemy.org/api";
+
 
 const getAuthorizationToken = () => {
   const auth = {
-    Authorization: localStorage.getItem("token")
-      ? `Bearer ${localStorage.getItem("token")}`
-      : null,
-  };
+    Authorization: localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : null
+  }
   return auth;
-};
+}
+
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
@@ -42,6 +42,11 @@ export const post = (route, payload) => {
   })
 }
 
-export const remove = (path, id) => {
-  return axiosInstance.delete(`${path}/${id}`);
-};
+export const put = (route, id, payload) => {
+  return axiosInstance.put(`${BASE_URL}/${route}/${id}`, payload,  {
+    headers: getAuthorizationToken(),
+  })
+}
+
+
+
