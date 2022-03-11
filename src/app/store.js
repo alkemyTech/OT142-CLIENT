6b4d31b0a2d39a6db59dfc628962f0e056a0acd4
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-03-11 01:20:39 -0300

    adding route reducer and reducer

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import categories from './features/ReducerCategories'
 import activitiesReducer from './features/activitiesSlice'
 import usersReducer from './features/UsersSlice'
 import slidesSlice from './features/slidesSlice'
+import commentsSlice from './features/comments'
 
 export default configureStore({
   reducer: {
