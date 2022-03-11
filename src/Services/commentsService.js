339f0b6dcff55341bfd8c6f2f7b224c76034eff9
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-03-11 01:22:43 -0300

    Fixing TYPO! 'ROUTE'

--- src/Services/commentsService.js
+++ src/Services/commentsService.js
@@ -7,19 +7,19 @@ export const getComment = (id) => {
 };
 
 export const postComment = (payload) => { 
-  return post(process.env.REACT_APP_COMMENTS, payload)
+  return post(process.env.REACT_APP_ROUTE_COMMENTS, payload)
     .then((res) => res.data)
     .catch((error) => console.log(error));
 }
 
 export const deleteComment = (id) => { 
-  return remove(process.env.REACT_APP_COMMENTS, id)
+  return remove(process.env.REACT_APP_ROUTE_COMMENTS, id)
     .then(res => res.data)
     .catch(error => console.log(error))
 }
 
 export const editComment = (id, payload) => { 
-  return put(process.env.REACT_APP_COMMENTS, id, payload)
+  return put(process.env.REACT_APP_ROUTE_COMMENTS, id, payload)
     .then(res => res.data)
     .catch(error => console.log(error))
 }
