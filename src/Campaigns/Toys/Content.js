7be8d2572c00826aabc1ec0eef6df27c868f8d94
joPorpatto <joel.porpatto@gmail.com>
2022-03-12 11:12:48 -0300

    branch inicialized

--- src/Campaigns/Toys/Content.js
+++ src/Campaigns/Toys/Content.js
@@ -9,3 +9,12 @@ const Content = () => {
 }
  
 export default Content;
+
+
+// Para dispositivos movil: descripción fecha, hora y lugar.
+
+// Para tablet: elementos anteriores + cuenta regresiva.
+
+// Para pc de escritorio: elementos anteriores + imágenes alrededor del contenido principal.
+
+// Para smart tv: elementos anteriores + dar color al fondo con transparencia.
