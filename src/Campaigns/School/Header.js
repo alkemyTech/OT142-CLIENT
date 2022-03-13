617c63d1edcf61535d07dad804ce7738ba036f4b
ncaruso <nico.caruso1992@outlook.com>
2022-03-13 10:18:22 -0300

    carpeta e imagen

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import { createBreakpoints } from '@chakra-ui/theme-tools'
+
+// This is the default breakpoint
+const breakpoints = createBreakpoints({
+  mobile: '320px',
+  tablet: '48em',
+  desktop: '62em',
+  smartTv: '80em',
+  '2xl': '96em',
+})
+
+//500px
 
 const Header = () => {
   return (
