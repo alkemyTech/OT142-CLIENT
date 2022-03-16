cf66db9b8cfc51fd2ed61c4f88d7f71ac64ddac6
Agustin <agus.ammazzagatti@gmail.com>
2022-03-16 16:19:53 -0300

    resolved Leaflet Maps

--- src/Components/Contact/ContactOngDate.js
+++ src/Components/Contact/ContactOngDate.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ContactDate from './ContactDate';
 import TitleContact from './TitleContact';
-export default function ContactOngDate () {
+import MapViews from './ContactMap/MapView';
+
+export default function ContactOngDate() {
   return (
-    <><div>
-    <TitleContact/>
-    <ContactDate/>
-    </div>
+    <>
+      <div>
+        <TitleContact />
+        <ContactDate />
+        <MapViews />
+      </div>
     </>
   );
 }
