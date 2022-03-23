66149eb2dfa93b4e5fa5fea92d8fdb6baac71986
Agustin <agus.ammazzagatti@gmail.com>
2022-03-22 21:16:45 -0300

    backoffice changed

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -34,7 +34,7 @@ const items = [
   },
   {
     id: 5,
-    title: 'Organization',
+    title: 'Organización',
     icon: <FaBeer />,
     desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     route: 'organization'
