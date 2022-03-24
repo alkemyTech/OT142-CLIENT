de8056559ef3e67e30dfc74734b94e559338b112
Nicolás Maximiliano Caruso <nico.caruso1992@outlook.com>
2022-03-24 16:07:58 -0300

    Searchbar funcional en pantalla actividades de public

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,9 +1,10 @@
-import { Text, Spinner, SimpleGrid, Box, GridItem } from '@chakra-ui/react';
+import { Text, Spinner, SimpleGrid, Box, GridItem, Input } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllActivities } from '../../app/features/activitiesSlice';
+import { getAllActivities, getOnChangeActivities } from '../../app/features/activitiesSlice';
 import { showAlertErr } from '../../Services/AlertServicie/AlertServicie';
 import Card from '../Card';
+import Searchbar from '../../utils/Searchbar';
 
 const ActivitiesList = () => {
   const dispatch = useDispatch();
@@ -13,10 +14,19 @@ const ActivitiesList = () => {
     dispatch(getAllActivities());
   }, [dispatch]);
 
+  const handleChange = (e) => {
+    if (e.target.value.length > 3) {
+      dispatch(getOnChangeActivities(e.target.value));
+    } else {
+      dispatch(getAllActivities());
+    }
+  };
+
   return (
         <Box bg='#F8F9FA' p={4} width="100%">
             <Text fontSize='5xl' d='flex' justifyContent='center'>Actividades</Text>
-
+            {/* <Input placeholder='Search' size={'lg'} w={'30%'} onChange={(e) => handleChange(e)}/> */}
+            <Searchbar handleChange={handleChange} />
             {activities.status !== 'success' && <Spinner color="blue" size='xl' />}
             {activities.status === 'failed' && showAlertErr()}
 
