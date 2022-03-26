b17b8d334c2b23bac991a132d22fff0b61d31b84
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-03-26 00:24:49 -0300

    backoffice NewsTable with debounce (do not merge into main)

--- src/Components/News/backoffice/NewsTable.js
+++ src/Components/News/backoffice/NewsTable.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import {
   Table,
   Thead,
@@ -6,48 +6,133 @@ import {
   Tr,
   Th,
   Text,
+  Input,
   Container,
   Box,
   Button,
-  Select
+  Select,
+  FormControl,
+  FormLabel
 } from '@chakra-ui/react';
 import TrTable from '../../Activities/backoffice/TrTable';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllNews } from '../../../app/features/newsSlice';
 
-const BackOfficeActivities = () => {
-  const dispatch = useDispatch();
-  const { news } = useSelector(state => state);
-  const [selectedValue, setSelectedValue] = useState('');
+const BackOfficeNews = () => {
+  const [newsData, setDataNews] = useState([]);
+  const [categorySelected, setCategorySelected] = useState('');
+  const [query, setQuery] = useState('');
+
+  const debounce = (func) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        timer = null;
+        func.apply(context, args);
+      }, 1000);
+    };
+  };
+
+  const debouncer = (handleChange) => {
+    return useCallback(debounce(handleChange), []);
+  };
+
   useEffect(() => {
-    dispatch(getAllNews());
-  }, [dispatch]);
+    if ((query === '' || query.length <= 2) && categorySelected === '') {
+      return fetch(
+        'https://ongapi.alkemy.org/api/news')
+        .then((r) => r.json())
+        .then((r) => {
+          const result = r.data;
+          setDataNews(result);
+        })
+        .catch((error) => {
+          console.error(error);
+          return [];
+        });
+    };
+  }, [query, categorySelected]);
 
-  console.log('value', selectedValue);
+  const handleChangeByName = (e) => {
+    const { value } = e.target;
+    setQuery(value);
+    if (value.length >= 3) {
+      fetch(
+        `https://ongapi.alkemy.org/api/news?search=${value}`)
+        .then((r) => r.json())
+        .then((r) => {
+          const result = r.data;
+          console.log('me ejecuto');
+          setDataNews(result);
+        })
+        .catch((error) => {
+          console.error(error);
+          return [];
+        });
+    }
+  };
+
+  const handleChangeByCategory = (e) => {
+    const { value } = e.target;
+    setCategorySelected(value);
+    if (value !== '' && value !== '5') {
+      fetch(
+        `https://ongapi.alkemy.org/api/news?category=${value}`)
+        .then((r) => r.json())
+        .then((r) => {
+          const result = r.data;
+          console.log('me ejecuto con cat');
+          setDataNews(result);
+        })
+        .catch((error) => {
+          console.error(error);
+          return [];
+        });
+    } else {
+      fetch(
+        'https://ongapi.alkemy.org/api/news')
+        .then((r) => r.json())
+        .then((r) => {
+          const result = r.data;
+          setDataNews(result);
+        })
+        .catch((error) => {
+          console.error(error);
+          return [];
+        });
+    }
+  };
+
+  useEffect(() => {
+  }, [newsData]);
 
   return (
      <Container maxW='100%'>
 
             <Box mb={5}>
-                <Text fontSize='6xl'>Backoffice de Actividades</Text>
+                <Text fontSize='6xl'>Backoffice de novedades</Text>
                 <Link to="/backoffice/activities/create">
                     <Button colorScheme='green'>
                         Crear nueva actividad
                     </Button>
                 </Link>
             </Box>
+            <FormControl>
+              <FormLabel>Búsqueda por nombre:</FormLabel>
+              <Input type='text' onChange={debouncer(handleChangeByName)} placeholder='Ingrese el nombre de la novdedad'/>
+            </FormControl>
             <Box>
               <Text>Selecione una categoría</Text>
-                <Select value={selectedValue} placeholder='Buscar por categoría' onChange={(e) => setSelectedValue(e.target.value)}>
-                  <option value='option1'>Categorías</option>
-                  <option value='option2'>Niños</option>
-                  <option value='option3'>Adolescente</option>
-                  <option value='option3'>Donaciones</option>
-                  <option value='option3'>Animales</option>
+                <Select placeholder='Buscar por categoría' onChange={debouncer(handleChangeByCategory)}>
+                  <option value='1'>Niños</option>
+                  <option value='2'>Adolescente</option>
+                  <option value='3'>Donaciones</option>
+                  <option value='4'>Animales</option>
+                  <option value='5'>Todas las categorías</option>
                 </Select>
             </Box>
-            {selectedValue === 'option1'
+            {newsData.length > 0
               ? <Table variant='simple'>
                 <Thead>
                     <Tr>
@@ -59,20 +144,20 @@ const BackOfficeActivities = () => {
                 </Thead>
                 <Tbody>
                     {
-                        news.news.map(activitie => {
+                        newsData.map(news => {
                           return <TrTable
-                                key={activitie.id}
-                                name={activitie.name}
-                                image={activitie.image}
-                                createdAt={activitie.created_at}
+                                key={news.id}
+                                name={news.name}
+                                image={news.image}
+                                createdAt={news.created_at}
                             />;
                         })
                     }
                 </Tbody>
             </Table>
-              : null };
+              : null }
         </Container>
   );
 };
 
-export default BackOfficeActivities;
+export default BackOfficeNews;
