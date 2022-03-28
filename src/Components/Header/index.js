365f42f2dae4fa40e48b9e0ba68203684efbdd82
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-03-28 12:41:10 -0300

    marginbotton and borderbottom deleted, HomeSlides Stack position from 150 to 180 top

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -46,14 +46,13 @@ export default function WithSubnavigation() {
   };
 
   return (
-    <Box mb={"20px"}>
+    <Box>
       <Flex
         bg={useColorModeValue("gray.300", "gray.800")}
         color={useColorModeValue("gray.600", "white")}
         minH={"60px"}
         py={{ base: 2 }}
         px={{ base: 4 }}
-        borderBottom={1}
         borderStyle={"solid"}
         borderColor={useColorModeValue("gray.200", "gray.900")}
         align={"center"}
