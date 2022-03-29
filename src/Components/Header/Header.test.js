5c352fd124455d1a6f94a8ccb9c57b206b197393
Nicolás Maximiliano Caruso <nico.caruso1992@outlook.com>
2022-03-29 16:58:50 -0300

    test para Header

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,28 +1,124 @@
+import {
+  render,
+  screen
+} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { fireEvent, render, waitFor, mount, screen } from '@testing-library/react';
 import WithSubnavigation from './index';
 import { MemoryRouter } from 'react-router-dom';
-import ReactDom from 'react-dom';
-import renderer from 'react-test-renderer';
 
-describe('Items de navegación', () => {
-  it('verificar si existe boton de ingresar', () => {
+describe('Items de navegación <WithSubNavigation/> sin logeo', () => {
+  sessionStorage.clear();
+
+  it('Verifica botón de login se visualice si no se está logueado', () => {
     render(
-      <MemoryRouter>
-        <WithSubnavigation />
-      </MemoryRouter>
+        <MemoryRouter>
+          <WithSubnavigation />
+        </MemoryRouter>
+    );
+    screen.logTestingPlaygroundURL();
+    expect(screen.getByRole('button', {
+      name: /ingresar/i
+    })).toBeInTheDocument();
+  }
+  );
+
+  it('Verifica link de Inicio se visualice si no se está logueado', () => {
+    render(
+        <MemoryRouter>
+          <WithSubnavigation />
+        </MemoryRouter>
     );
 
-    expect(screen.getByText('Ingresar')).toBeInTheDocument();
-  });
+    expect(screen.getByRole('link', {
+      name: /inicio/i
+    })).toBeInTheDocument();
+  }
+  );
 
-  it('verificar si no existe boton de backoffice', () => {
+  it('Verifica link de Nosotros se visualice si no se está logueado', () => {
     render(
-      <MemoryRouter>
-        <WithSubnavigation />
-      </MemoryRouter>
+        <MemoryRouter>
+          <WithSubnavigation />
+        </MemoryRouter>
+    );
+    expect(screen.getByRole('link', {
+      name: /nosotros/i
+    })).toBeInTheDocument();
+  }
+  );
+
+  it('Verifica link de Actividades se visualice si no se está logueado', () => {
+    render(
+        <MemoryRouter>
+          <WithSubnavigation />
+        </MemoryRouter>
+    );
+    expect(screen.getByRole('link', {
+      name: /actividades/i
+    })).toBeInTheDocument();
+  }
+  );
+
+  it('Verifica link de Contacto se visualice si no se está logueado', () => {
+    render(
+        <MemoryRouter>
+          <WithSubnavigation />
+        </MemoryRouter>
+    );
+    expect(screen.getByRole('link', {
+      name: /contacto/i
+    })).toBeInTheDocument();
+  }
+  );
+
+  it('Verifica link de Campañas se visualice si no se está logueado', () => {
+    render(
+        <MemoryRouter>
+          <WithSubnavigation />
+        </MemoryRouter>
+    );
+    expect(screen.getByRole('link', {
+      name: /campañas/i
+    })).toBeInTheDocument();
+  }
+  );
+});
+
+describe('Items de navegacion <WithSubNavigation/> con logeo', () => {
+  it('Verifica que el boton ingresar no se visualice si está logueado y no es admin', () => {
+    sessionStorage.setItem('login-token', 'true');
+    sessionStorage.setItem('login-role', '0');
+    render(
+        <MemoryRouter>
+          <WithSubnavigation />
+        </MemoryRouter>
+    );
+    expect(screen.queryByText('Ingresar')).not.toBeInTheDocument();
+    sessionStorage.clear();
+  }
+  );
+
+  it('Verifica que el boton ingresar no se visualice si está logueado', () => {
+    sessionStorage.setItem('login-token', 'true');
+    sessionStorage.setItem('login-role', '0');
+    render(
+        <MemoryRouter>
+          <WithSubnavigation />
+        </MemoryRouter>
     );
 
-    expect(screen.queryByText('Backoffice')).not.toBeInTheDocument();
-  });
+    expect(screen.queryByText('Ingresar')).not.toBeInTheDocument();
+    sessionStorage.clear();
+  }
+  );
+});
+
+describe('Items de navegacion <WithSubNavigation/> con logeo y admin', () => {
+  sessionStorage.setItem('login-token', 'true');
+  sessionStorage.setItem('login-role', '1');
+  it('Verifica que el boton  contacto  no se visualice si está logueado y es admin', () => {
+    expect(screen.queryByText('Contacto')).not.toBeInTheDocument();
+  }
+  );
+  sessionStorage.clear();
 });
